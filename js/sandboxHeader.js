641de3e2ba13966d8aa627b23989c29ddeb3d760
Pepper-Wood <dipippo.k@gmail.com>
2018-01-03 02:02:43 -0500

    Migrated Trade Center Changes
    Homepage:
    - editted header bar and titles to use official Soosh brand fonts
    - added "last updated" text to index
    - added official logo
    
    Trade Center:
    - changed styling to use flexboxes instead of bootstrap grids
    - added category for cave sooshes
    - add option to filter by dog type
    - incorporated WitchHex's database so that the trade spreadsheet only needs a soosh's name and not its URL and image URL all the time

--- js/sandboxHeader.js
+++ js/sandboxHeader.js
@@ -1,28 +1,38 @@
 console.log(window.location.href);
 $("#headerSection").html(
     '<div class="sidebar">'
-    + '    <span class="sidebarTitle">Soosh Repositories</span>'
+    + '    <h2 class="sidebarTitle">Soosh Repositories</h2>'
     + '    <ul class="sidebarNav">'
     + '        <li id="sidenavIndex"><a href="index.html">Home</a></li>'
-    + '        <li id="sidenavSoosh"><a href="http://www.kathryndipippo.com/WebDevSandbox/sooshTraitVisualizer.html">Soosh Trait Visualizer</a></li>'
-    + '        <li id="sidenavPersonality"><a href="http://www.kathryndipippo.com/SooshARPG/kumo.html">SooshARPG - Kumo Tracker</a></li>'
-    + '        <li id="sidenavSprite"><a href="http://www.kathryndipippo.com/SooshARPG/mod.html">SooshARPG - Moderator Tools</a></li>'
-    + '        <li id="sidenavData"><a href="http://www.kathryndipippo.com/SooshARPG2/">SooshARPG 2.0</a></li>'
-    + '        <li id="sidenavSalem"><a href="http://www.kathryndipippo.com/SooshARPG/tradecenter.html">Soosh Trade Center</a></li>'
-    + '        <li id="sidenav"><a href="http://www.kathryndipippo.com/SooshARPG/database.html">Soosh Database</a></li>'
-    + '        <li id="sidenav"><a href="http://www.kathryndipippo.com/SooshARPG/witchhex.html">WitchHex DB</a></li>'
+    + '        <li id="sidenavTradeCenter"><a href="tradecenter.html">Soosh Trade Center</a></li>'
+    + '        <li><a href="http://www.kathryndipippo.com/WebDevSandbox/sooshTraitVisualizer.html">Soosh Trait Visualizer</a></li>'
+    + '        <li><a href="http://www.kathryndipippo.com/SooshARPG/kumo.html">SooshARPG - Kumo Tracker</a></li>'
+    + '        <li><a href="http://www.kathryndipippo.com/SooshARPG/mod.html">SooshARPG - Moderator Tools</a></li>'
+    + '        <li><a href="http://www.kathryndipippo.com/SooshARPG2/">SooshARPG 2.0</a></li>'
+    + '        <li><a href="http://www.kathryndipippo.com/SooshARPG/database.html">Soosh Database</a></li>'
     + '    </ul>'
     + '</div>'
     + '<div class="headerOverlay"></div>'
     + '<header>'
     + '    <div><i id="sidebarTrigger" class="material-icons">dehaze</i></div>'
-    + '    <div><a class="headerLink" href="index.html">'
-    + '        <img class="headerIcon" src="img/favicon.png">'
-    + '        Sushi Dogs'
-    + '    </a></div>'
+    + '    <div><img class="headerIcon" src="img/favicon.png"></div>'
+    + '    <div><a class="headerLink" href="index.html">Sushi Dogs: <span class="headerMarker">Pepper-Wood\'s Tools</span></a></div>'
     + '</header>'
     + '<div class="headerblankspace"></div>'
 );
+
+function addActiveTab() {
+    var sidenavTabs = ["Index","TradeCenter"];
+    var currentPage = window.location.href;
+    for (i=0; i<sidenavTabs.length; i++) {
+        if (currentPage.includes(sidenavTabs[i].toLowerCase())) {
+            $("#sidenav" + sidenavTabs[i]).addClass("active");
+            return;
+        }
+    }
+}
+
+addActiveTab();
 
 $("#sidebarTrigger").click(function() {
     $(".headerOverlay").fadeIn();
