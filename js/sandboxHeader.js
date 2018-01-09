8d05c8241eca45c467606a5afc3ea33a04e9c1ae
Pepper-Wood <dipippo.k@gmail.com>
2018-01-09 00:03:28 -0500

    Trade Center updates and Index styling
    - changed header to be slightly transparent to not have the paw-print background be overbearing
    - Added "Comments and Suggestions"
    - reworked filter logic in Trade Center
    - added rarity filter to Trade Center and logic with the official date

--- js/sandboxHeader.js
+++ js/sandboxHeader.js
@@ -1,15 +1,20 @@
-console.log(window.location.href);
 $("#headerSection").html(
     '<div class="sidebar">'
     + '    <h2 class="sidebarTitle">Soosh Repositories</h2>'
     + '    <ul class="sidebarNav">'
     + '        <li id="sidenavIndex"><a href="index.html">Home</a></li>'
-    + '        <li id="sidenavTradeCenter"><a href="tradecenter.html">Soosh Trade Center</a></li>'
+    + '        <li><hr></li>'
+    + '        <li class="sidenavSmallTitle">Ongoing</li>'
+    + '        <li id="sidenavTradeCenter"><a href="tradecenter.html">Visual Soosh Trade Center</a></li>'
+    + '        <li><a href="http://www.kathryndipippo.com/SooshARPG2/">SooshARPG 2.0</a></li>'
     + '        <li><a href="http://www.kathryndipippo.com/WebDevSandbox/sooshTraitVisualizer.html">Soosh Trait Visualizer</a></li>'
+    + '        <li><hr></li>'
+    + '        <li class="sidenavSmallTitle">Retired</li>'
     + '        <li><a href="http://www.kathryndipippo.com/SooshARPG/kumo.html">SooshARPG - Kumo Tracker</a></li>'
     + '        <li><a href="http://www.kathryndipippo.com/SooshARPG/mod.html">SooshARPG - Moderator Tools</a></li>'
-    + '        <li><a href="http://www.kathryndipippo.com/SooshARPG2/">SooshARPG 2.0</a></li>'
     + '        <li><a href="http://www.kathryndipippo.com/SooshARPG/database.html">Soosh Database</a></li>'
+    + '        <li><hr></li>'
+    + '        <li><a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSe-jNHbh_gOAOS0LbSvC65YiC-BJVunmck3D0kt-W6OMZBAMg/viewform?usp=sf_link">Comments and Suggestions</a></li>'
     + '    </ul>'
     + '</div>'
     + '<div class="headerOverlay"></div>'
@@ -18,7 +23,6 @@ $("#headerSection").html(
     + '    <div><img class="headerIcon" src="img/favicon.png"></div>'
     + '    <div><a class="headerLink" href="index.html">Sushi Dogs: <span class="headerMarker">Pepper-Wood\'s Tools</span></a></div>'
     + '</header>'
-    + '<div class="headerblankspace"></div>'
 );
 
 function addActiveTab() {
